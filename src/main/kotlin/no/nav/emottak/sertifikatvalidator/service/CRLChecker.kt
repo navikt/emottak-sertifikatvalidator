3a23a477f3b97b9cd0500703b0e863dbc7acbaee
Thomas Burnett <thomas.hoberg.burnett@nav.no>
2021-10-01 14:01:31 +0200

    Tester commfides crl

--- src/main/kotlin/no/nav/emottak/sertifikatvalidator/service/CRLChecker.kt
+++ src/main/kotlin/no/nav/emottak/sertifikatvalidator/service/CRLChecker.kt
@@ -105,9 +105,8 @@ class CRLChecker {
             //.accept(MediaType("application", "pkix-crl"), MediaType("application", "x-pkcs7-crl"))
             .retrieve()
             .bodyToMono(ByteArray::class.java)
-            .timeout(Duration.ofSeconds(30))
             .retryWhen(Retry.backoff(3, Duration.ofSeconds(3)))
-        return response.block()?.inputStream() ?: throw SertifikatError(HttpStatus.INTERNAL_SERVER_ERROR, "$crlUrl: Feil ved henting av CRL fra URL")
+        return response.block(Duration.ofSeconds(30))?.inputStream() ?: throw SertifikatError(HttpStatus.INTERNAL_SERVER_ERROR, "$crlUrl: Feil ved henting av CRL fra URL")
     }
 
     private fun createCRL(input: InputStream?): X509CRL {
